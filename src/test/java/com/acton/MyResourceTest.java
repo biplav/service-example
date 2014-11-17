a8aedb8f35bb69bbf1edea9892f8b883da4acf1a
biplav saraf <biplav.saraf@example.com>
2014-11-18 02:50:17 +0530

    empty test

--- src/test/java/com/acton/MyResourceTest.java
+++ src/test/java/com/acton/MyResourceTest.java
@@ -1,15 +1,10 @@
 package com.acton;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class MyResourceTest {
 
@@ -18,23 +13,11 @@ public class MyResourceTest {
 
     @Before
     public void setUp() throws Exception {
-        // start the server
-        server = Main.startServer();
-        // create the client
-        Client c = ClientBuilder.newClient();
-
-        // uncomment the following line if you want to enable
-        // support for JSON in the client (you also have to uncomment
-        // dependency on jersey-media-json module in pom.xml and Main.startServer())
-        // --
-        // c.configuration().enable(new org.glassfish.jersey.media.json.JsonJaxbFeature());
-
-        target = c.target(Main.BASE_URI);
     }
 
     @After
     public void tearDown() throws Exception {
-        server.stop();
+
    }
 }
 
