9f82a21dcc13f722627ae38e8820a806b8e59374
biplav saraf <biplav.saraf@example.com>
2014-11-18 02:48:06 +0530

    port setting

--- src/main/java/com/acton/Main.java
+++ src/main/java/com/acton/Main.java
@@ -1,35 +1,40 @@
 package com.acton;
 
+import java.io.IOException;
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
-
-import java.io.IOException;
-import java.net.URI;
 
 /**
  * Main class.
  *
  */
 public class Main {
-	// Base URI the Grizzly HTTP server will listen on
-	private static final String HOSTNAME = System.getenv("HOSTNAME");
-	private static final String PORT = System.getenv("PORT");
-	public static final String BASE_URI = HOSTNAME == null ? "http://localhost:8080/"
-			: "http://" + HOSTNAME + ":" + PORT + "/";
+	
+	public static final String PORT_ENV = System.getenv("PORT");
+	public static final String PORT = PORT_ENV == null ? "8080" : PORT_ENV;
+	
+	 private static URI getBaseURI(int port) {
+	        return UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
+	    }
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @param uri 
      * @return Grizzly HTTP server.
      */
-    public static HttpServer startServer() {
+    public static HttpServer startServer(URI uri) {
         // create a resource config that scans for JAX-RS resources and providers
         // in com.acton package
         final ResourceConfig rc = new ResourceConfig().packages("com.acton");
         rc.register(CORSResponseFilter.class);
         // create and start a new instance of grizzly http server
         // exposing the Jersey application at BASE_URI
-        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return GrizzlyHttpServerFactory.createHttpServer(uri, rc);
     }
 
     /**
@@ -38,9 +43,10 @@ public class Main {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        final HttpServer server = startServer();
+    	URI uri = getBaseURI(Integer.valueOf(PORT));
+        final HttpServer server = startServer(uri);
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+                + "%sapplication.wadl\nHit enter to stop it...", uri));
         while(true) {
             System.in.read();
         }
